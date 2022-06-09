42afa493c19f3954a814f59b1dc05d14d175cbaf
peiheng <s1948149@ed.ac.uk>
2022-06-09 10:01:19 +0100

    added pymysql

--- data/upload.py
+++ data/upload.py
@@ -1,6 +1,14 @@
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
+import pymysql
+
 
 folder = askdirectory(title='Which folder are we uploading?') # select the csv data folder
 
 print(folder)
+
+# Creating connection
+db = pymysql.connect('dynamic-foraging-data.cqmwfsljtplu.eu-west-2.rds.amazonaws.com', 'admin', 'Luph65588590-')
+cursor = db.cursor()
+
+cursor.execute('select version()')
