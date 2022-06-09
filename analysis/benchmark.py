eff0b9c38853b2dc5ee1a2f72edc11137c6f3b92
PeihengLu <s1948149@ed.ac.uk>
2022-06-09 19:19:54 +0100

    add analysis section

---
+++ analysis/benchmark.py
@@ -0,0 +1,8 @@
+# benchmark the performance of a mouse
+# determining if the mouse can be considered trained
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-p', '--path', help='path to performance csv file in benchmark task')
+
